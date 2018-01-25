c7924ace7670d6d5b2860d91a494cd11126b879d
Jeremy Magland <jeremy.magland@gmail.com>
2018-01-25 13:30:20 -0500

    fix

--- ml_phret_spec.m
+++ ml_phret_spec.m
@@ -7,14 +7,15 @@ addpath([curpath,'/jsonlab-1.5']);
 processors={};
 
 pfunctions={...
-    @p_saveimage,...
-    @p_create_phantom2d...
+    'p_saveimage',...
+    'p_create_phantom2d'...
 };
 
 for ii=1:length(pfunctions)
-    FF=pfunctions{ii};
-    spec0=FF('spec');
-    spec0.exe_command=create_exe_command(spec0,curpath);
+    func_name=pfunctions{ii};
+    func=str2func(func_name);
+    spec0=func('spec');
+    spec0.exe_command=create_exe_command(func_name,spec0,curpath);
     processors{end+1}=spec0;
 end;
 
@@ -23,7 +24,7 @@ spec=struct('processors',{processors});
 json=savejson('',spec);
 disp(json);
 
-function exe_command=create_exe_command(spec0,curpath)
+function exe_command=create_exe_command(func_name,spec0,curpath)
 
 pnames={};
 for ii=1:length(spec0.inputs)
@@ -39,11 +40,11 @@ end;
 struct_string='';
 for ii=1:length(pnames)
     pname=pnames{ii};
-    if (ii>0)
+    if (ii>1)
         struct_string=[struct_string,','];
     end;
     struct_string=[struct_string,"'",pname,"','$",pname,"$'"];
 end;
 
-str=sprintf("addpath('%s/processors'); p_saveimage(struct(%s));",curpath,struct_string);
+str=sprintf("addpath('%s/processors'); %s(struct(%s));",curpath,func_name,struct_string);
 exe_command=sprintf('octave --eval "%s"',str);
